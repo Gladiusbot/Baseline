51727963b1cc1ea2ed2274f315110fbc417df803
Gladiator1218 <g2007531218@hotmail.com>
2020-06-16 19:25:42 +0800

    - init commit

---
+++ my_utils/augmentations.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+
+def horisontal_flip(images, targets):
+    images = torch.flip(images, [-1])
+    targets[:, 2] = 1 - targets[:, 2]
+    return images, targets
+
+def vertical_flip(images, targets):
+    images = torch.flip(images, [-2])
+    targets[:, 3] = 1 - targets[:, 3]
+    return images, targets
---
+++ my_utils/datasets.py
@@ -0,0 +1,205 @@
+import glob
+import random
+import os
+import sys
+import numpy as np
+from PIL import Image
+import torch
+import torch.nn.functional as F
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from my_utils.augmentations import horisontal_flip, vertical_flip
+from torch.utils.data import Dataset
+import torchvision.transforms as transforms
+
+#for test
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
+def pad_to_square(img, pad_value):
+    c, h, w = img.shape
+    dim_diff = np.abs(h - w)
+    # (upper / left) padding and (lower / right) padding
+    pad1, pad2 = dim_diff // 2, dim_diff - dim_diff // 2
+    # Determine padding
+    pad = (0, 0, pad1, pad2) if h <= w else (pad1, pad2, 0, 0)
+    # Add padding
+    img = F.pad(img, pad, "constant", value=pad_value)
+
+    return img, pad
+
+
+def resize(image, size):
+    image = F.interpolate(image.unsqueeze(0), size=size, mode="nearest").squeeze(0)
+    return image
+
+
+def random_resize(images, min_size=288, max_size=448):
+    new_size = random.sample(list(range(min_size, max_size + 1, 32)), 1)[0]
+    images = F.interpolate(images, size=new_size, mode="nearest")
+    return images
+
+
+class ImageFolder(Dataset):
+    def __init__(self, folder_path, img_size=416):
+        self.files = sorted(glob.glob("%s/*.*" % folder_path))
+        self.img_size = img_size
+
+    def __getitem__(self, index):
+        img_path = self.files[index % len(self.files)]
+        # Extract image as PyTorch tensor
+        img = transforms.ToTensor()(Image.open(img_path))
+        # Pad to square resolution
+        img, _ = pad_to_square(img, 0)
+        # Resize
+        img = resize(img, self.img_size)
+
+        return img_path, img
+
+    def __len__(self):
+        return len(self.files)
+
+
+class ListDataset(Dataset):
+    def __init__(self, list_path, img_size=416, augment=True, multiscale=True, normalized_labels=True):
+        with open(list_path, "r") as file:
+            self.img_files = file.readlines()
+
+        self.label_files = [
+            path.replace("images", "labels").replace(".png", ".txt").replace(".jpg", ".txt")
+            for path in self.img_files
+        ]
+        self.img_size = img_size
+        self.max_objects = 100
+        self.augment = augment
+        self.multiscale = multiscale
+        self.normalized_labels = normalized_labels
+        self.min_size = self.img_size - 3 * 32
+        self.max_size = self.img_size + 3 * 32
+        self.batch_count = 0
+
+    def __getitem__(self, index):
+
+        # ---------
+        #  Image
+        # ---------
+
+        img_path = self.img_files[index % len(self.img_files)].rstrip()
+
+        # Extract image as PyTorch tensor
+        img = transforms.ToTensor()(Image.open(img_path).convert('RGB'))
+
+        # Handle images with less than three channels
+        if len(img.shape) != 3:
+            img = img.unsqueeze(0)
+            img = img.expand((3, img.shape[1:]))
+
+        _, h, w = img.shape
+        h_factor, w_factor = (h, w) if self.normalized_labels else (1, 1)
+        # Pad to square resolution
+        img, pad = pad_to_square(img, 0)
+        _, padded_h, padded_w = img.shape
+
+        # ---------
+        #  Label
+        # ---------
+
+        label_path = self.label_files[index % len(self.img_files)].rstrip()
+
+        targets = None
+        if os.path.exists(label_path):
+            boxes = torch.from_numpy(np.loadtxt(label_path).reshape(-1, 5))
+            # Extract coordinates for unpadded + unscaled image
+            x1 = w_factor * (boxes[:, 1] - boxes[:, 3] / 2)
+            y1 = h_factor * (boxes[:, 2] - boxes[:, 4] / 2)
+            x2 = w_factor * (boxes[:, 1] + boxes[:, 3] / 2)
+            y2 = h_factor * (boxes[:, 2] + boxes[:, 4] / 2)
+            # Adjust for added padding
+            x1 += pad[0]
+            y1 += pad[2]
+            x2 += pad[1]
+            y2 += pad[3]
+            # Returns (x, y, w, h)
+            boxes[:, 1] = ((x1 + x2) / 2) / padded_w
+            boxes[:, 2] = ((y1 + y2) / 2) / padded_h
+            boxes[:, 3] *= w_factor / padded_w
+            boxes[:, 4] *= h_factor / padded_h
+
+            targets = torch.zeros((len(boxes), 6))
+            targets[:, 1:] = boxes
+
+        # Apply augmentations
+        # plot_img_with_bbox(img, targets)
+        if self.augment:
+            rnd = np.random.random()
+            if rnd < 0.33:
+                # print("horisontal_flip")
+                img, targets = horisontal_flip(img, targets)
+            elif rnd < 0.66:
+                # print("vertical_flip")
+                img, targets = vertical_flip(img, targets)
+        # plot_img_with_bbox(img, targets)
+
+        return img_path, img, targets
+
+    def collate_fn(self, batch):
+        paths, imgs, targets = list(zip(*batch))
+        # Remove empty placeholder targets
+        targets = [boxes for boxes in targets if boxes is not None]
+        # Add sample index to targets
+        for i, boxes in enumerate(targets):
+            boxes[:, 0] = i
+        targets = torch.cat(targets, 0)
+        # Selects new image size every tenth batch
+        if self.multiscale and self.batch_count % 10 == 0:
+            self.img_size = random.choice(range(self.min_size, self.max_size + 1, 32))
+        # Resize images to input shape
+        imgs = torch.stack([resize(img, self.img_size) for img in imgs])
+        self.batch_count += 1
+        return paths, imgs, targets
+
+    def __len__(self):
+        return len(self.img_files)
+
+def plot_img_with_bbox(img, targets):
+    # img: 3, h, w tensor
+    fig, ax = plt.subplots(1)
+    tensor_img = img.permute(1, 2, 0)
+    ax.imshow(tensor_img)
+    # calc bbox and add patch
+    for target in targets:
+        w = img.shape[2] * target[4]
+        h = img.shape[1] * target[5]
+        x = img.shape[2] * target[2] - w // 2
+        y = img.shape[1] * target[3] - h // 2
+        rect = patches.Rectangle((x,y),w,h,linewidth=1,edgecolor='r',facecolor='none')
+        ax.add_patch(rect)
+    # plt img and bbox
+    plt.show()
+
+if __name__ == '__main__':
+
+    train_path = r"../config/train.txt"
+    dataset = ListDataset(
+        train_path, 
+        img_size=416, 
+        augment=True, 
+        multiscale=True
+    )
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=1,
+        shuffle=True,
+        num_workers=1,
+        pin_memory=True,
+        collate_fn=dataset.collate_fn,
+    )
+    i = 0
+    for batch_i, (path, imgs, targets) in enumerate(dataloader):
+        # print("imgs:",imgs)
+        # print("targets:",targets)
+        # print("path:",path)
+        # plot_img_with_bbox(imgs[0], targets[0])
+        # pass
+        break
